87f1bffd03c3b36a2680c3cc141560fad45c1957
Federico Galatolo <galatolo.federico@gmail.com>
2023-03-11 13:15:53 +0100

    removed testing main

--- inference.py
+++ inference.py
@@ -53,14 +53,3 @@ class LLaMAInference:
             top_p=top_p
         )
         return results
-
-if __name__ == "__main__":
-    models = ["7B", "13B", "30B", "65B"]
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--llama-path", type=str, required=True)
-    parser.add_argument("--model", type=str, required=True, choices=models)
-    args = parser.parse_args()
-
-    llama = LLaMAInference(args.llama_path, args.model)
-    llama.generate(["My name is Kyle"])
-    
