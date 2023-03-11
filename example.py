e5b4f551c4b2ea02941945c2e2c4f4d534c61346
Federico Galatolo <galatolo.federico@gmail.com>
2023-03-11 16:03:32 +0100

    added timings

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import time
 import argparse
 from inference import LLaMAInference
 
@@ -7,6 +8,11 @@ if __name__ == "__main__":
     parser.add_argument("--llama-path", type=str, required=True)
     parser.add_argument("--model", type=str, required=True, choices=models)
     args = parser.parse_args()
-
+    
+    start_loading = time.time()
     llama = LLaMAInference(args.llama_path, args.model)
+    print(f"Loaded model {args.model} in {time.time() - start_loading:.2f} seconds")
+
+    start_generation = time.time()
     print(llama.generate(["My name is Federico"]))
+    print(f"Inference took {time.time() - start_generation:.2f} seconds")
